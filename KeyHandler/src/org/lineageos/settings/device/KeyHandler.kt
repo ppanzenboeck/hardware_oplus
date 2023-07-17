dc6c305ca28bbb13de4a38955a10cbf0bf66b48a
Nick Reuter <nreuter85@gmail.com>
2023-07-16 21:31:56 -0500

    KeyHandler: Initial support for UI notifications
    This is a port of https://gerrit.aospa.co/25761, reworked into an
    OverlayPlugin from SystemUI. Updates are broadcasted to ensure correct
    position and information.
    Available states: Off, Show (Left), Show (Right).
    
    idoybh (YAAP):
    * Adapted to our DeviceSettings
    * Follow day/night UI mode
    * Positioning, movement and other visual improvements
    * Got rid of some unrelated deprecations while at it
    
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Change-Id: Ibeb50e7e24c3d6130170febaf3e4a5233a829e52

--- KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
+++ KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
@@ -15,6 +15,7 @@ import android.media.AudioSystem
 import android.os.VibrationAttributes
 import android.os.VibrationEffect
 import android.os.Vibrator
+import android.os.VibratorManager
 import android.provider.Settings
 import android.view.KeyEvent
 import com.android.internal.os.DeviceKeyHandler
@@ -54,10 +55,13 @@ class KeyHandler : DeviceKeyHandler {
     constructor(context: Context) : this() {
         audioManager = context.getSystemService(AudioManager::class.java)!!
         notificationManager = context.getSystemService(NotificationManager::class.java)!!
-        vibrator = context.getSystemService(Vibrator::class.java)!!
+
+        // Update to use VibratorManager for compatibility
+        val vibratorManager = context.getSystemService(VibratorManager::class.java)
+        vibrator = vibratorManager?.defaultVibrator ?: context.getSystemService(Vibrator::class.java)!!
 
         packageContext = context.createPackageContext(
-            KeyHandler::class.java.getPackage()!!.name, 0
+            KeyHandler::class.java.packageName, 0
         )
         sharedPreferences = packageContext.getSharedPreferences(
             packageContext.packageName + "_preferences",
@@ -151,7 +155,16 @@ class KeyHandler : DeviceKeyHandler {
             if (vibrate) {
                 vibrateIfNeeded(mode)
             }
+            sendNotification(position, mode)
         }
+    }
+
+    private fun sendNotification(position: Int, mode: Int) {
+        val intent = Intent(SLIDER_UPDATE_ACTION).apply {
+            putExtra("position", position)
+            putExtra("mode", mode)
+        }
+        packageContext.sendBroadcast(intent)
     }
 
     private fun setZenMode(zenMode: Int) {
@@ -168,9 +181,11 @@ class KeyHandler : DeviceKeyHandler {
         private const val TAG = "KeyHandler"
 
         // Slider key positions
-        private const val POSITION_TOP = 1
-        private const val POSITION_MIDDLE = 2
-        private const val POSITION_BOTTOM = 3
+        const val POSITION_TOP = 1
+        const val POSITION_MIDDLE = 2
+        const val POSITION_BOTTOM = 3
+
+        const val SLIDER_UPDATE_ACTION = "org.lineageos.settings.device.UPDATE_SLIDER"
 
         // Preference keys
         private const val ALERT_SLIDER_TOP_KEY = "config_top_position"
