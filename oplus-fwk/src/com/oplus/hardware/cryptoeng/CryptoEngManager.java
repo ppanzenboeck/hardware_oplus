462b1b1ecb5c62a62277cfcb6cc55fa2e4dd16ed
Jonas Salo <salomannen1234@gmail.com>
2026-06-22 18:59:12 +0200

    oplus-fwk: wire App Lock and cryptoeng services
    Change-Id: I44be825ef4c5797b7fad35068c1ffd525d05005e

--- oplus-fwk/src/com/oplus/hardware/cryptoeng/CryptoEngManager.java
+++ oplus-fwk/src/com/oplus/hardware/cryptoeng/CryptoEngManager.java
@@ -1,7 +1,29 @@
 package com.oplus.hardware.cryptoeng;
 
+import android.os.IBinder;
+import android.os.Parcel;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.util.Log;
+
 public class CryptoEngManager {
+    private static final String TAG = "CryptoEngManager";
+    private static final String DESCRIPTOR = "vendor.oplus.hardware.cryptoeng.ICryptoeng";
+    private static final int TRANSACTION_CRYPTOENG_INVOKE_COMMAND = IBinder.FIRST_CALL_TRANSACTION;
     private static volatile CryptoEngManager sInstance;
+    private static final String SERVICE_NAME = DESCRIPTOR + "/default";
+
+    private volatile IBinder mCryptoEngService;
+    private final IBinder.DeathRecipient mDeathRecipient =
+            new IBinder.DeathRecipient() {
+                @Override
+                public void binderDied() {
+                    Log.i(TAG, SERVICE_NAME + " binderDied");
+                    synchronized (CryptoEngManager.class) {
+                        mCryptoEngService = null;
+                    }
+                }
+            };
 
     private CryptoEngManager() {
     }
@@ -17,8 +39,45 @@ public class CryptoEngManager {
         return sInstance;
     }
 
+    private synchronized IBinder getService() {
+        if (mCryptoEngService == null) {
+            IBinder binder = ServiceManager.getService(SERVICE_NAME);
+            if (binder == null) {
+                Log.w(TAG, "getService fail. " + SERVICE_NAME);
+                return null;
+            }
+            try {
+                binder.linkToDeath(mDeathRecipient, 0);
+                mCryptoEngService = binder;
+            } catch (RemoteException e) {
+                Log.e(TAG, "linkToDeath fail", e);
+                return null;
+            }
+        }
+        return mCryptoEngService;
+    }
+
     public byte[] cryptoEngCommand(byte[] inData) {
-        return null;
+        IBinder service = getService();
+        if (service == null) {
+            return null;
+        }
+
+        Parcel data = Parcel.obtain();
+        Parcel reply = Parcel.obtain();
+        try {
+            data.writeInterfaceToken(DESCRIPTOR);
+            data.writeByteArray(inData);
+            service.transact(TRANSACTION_CRYPTOENG_INVOKE_COMMAND, data, reply, 0);
+            reply.readException();
+            return reply.createByteArray();
+        } catch (RemoteException e) {
+            Log.e(TAG, "cryptoEngCommand failed", e);
+            return null;
+        } finally {
+            reply.recycle();
+            data.recycle();
+        }
     }
 
     public static class CommandId {
